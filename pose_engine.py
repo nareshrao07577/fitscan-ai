2d2d7bb9143320b04a788f7974fcc27e7d828774
Naresh Yadav <nareshrao07577@gmail.com>
2025-09-14 12:57:59 +0530

    Create pose_engine.py

---
+++ pose_engine.py
@@ -0,0 +1,8 @@
+def get_keypoints(frame_bgr):
+    """
+    Stub function for pose engine.
+    Returns a dict of keypoints with format:
+      {"left_hip": (x,y,conf), ...}
+    For now, returns an empty dict to let the scaffold run.
+    """
+    return {}
